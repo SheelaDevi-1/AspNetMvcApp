36663aacb293945085be9dd885c209b48f942cea
sheeladevi <sheelu.sep1@gmail.com>
2025-08-02 11:51:48 -0600

    added caurosel

--- Automobile Speed/obj/Debug/net8.0/Automobile Speed.AssemblyInfo.cs
+++ Automobile Speed/obj/Debug/net8.0/Automobile Speed.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Automobile Speed")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a672ddea445f569e84d7c35364dabda52d60938")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0206626e774d13dbc2683733c778d8225e8d807f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Automobile Speed")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Automobile Speed")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
